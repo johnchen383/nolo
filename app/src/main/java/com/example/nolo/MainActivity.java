503b3b4fbec0d9caa5ea1697c038ee3deca02152
johnchen383 <john.bm.chen@gmail.com>
2022-08-08 18:33:59 +1200

    feat: transparent bottom nav + removing title bar

--- app/src/main/java/com/example/nolo/MainActivity.java
+++ app/src/main/java/com/example/nolo/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
