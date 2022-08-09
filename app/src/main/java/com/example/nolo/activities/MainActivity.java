1d3df7b8f48e85b7a76998b8b0acaa8e69c98082
johnchen383 <john.bm.chen@gmail.com>
2022-08-09 12:18:03 +1200

    feat: added viewholder for activity

--- app/src/main/java/com/example/nolo/activities/MainActivity.java
+++ app/src/main/java/com/example/nolo/activities/MainActivity.java
@@ -12,6 +12,16 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewHolder vh;
+
+    private class ViewHolder {
+        BottomNavigationView navView;
+
+        public ViewHolder(){
+            navView = findViewById(R.id.nav_view);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +31,17 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        vh = new ViewHolder();
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_search, R.id.navigation_cart, R.id.navigation_profile)
                 .build();
-
+        
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(vh.navView, navController);
     }
 
 }
