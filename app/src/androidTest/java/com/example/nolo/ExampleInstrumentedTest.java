2916083b42e8c630b9db762ac156d257f62da70e
johnchen383 <john.bm.chen@gmail.com>
2022-08-12 19:09:40 +1200

    test: added integration tests for loading stores + implemented loading

--- app/src/androidTest/java/com/example/nolo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nolo/ExampleInstrumentedTest.java
@@ -5,10 +5,21 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import com.example.nolo.interactors.LoadStoresRepositoryUseCase;
+import com.example.nolo.repositories.store.StoresRepository;
+import com.google.firebase.FirebaseApp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +28,29 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    Context appContext;
+    CountDownLatch lock = new CountDownLatch(1);
+
+    @Before
+    public void setUp(){
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        FirebaseApp.initializeApp(appContext);
+    }
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    public void testPackageName() {
         assertEquals("com.example.nolo", appContext.getPackageName());
+    }
+
+    @Test
+    public void testLoadingStoresData() throws Exception {
+        List<Class<?>> str = new ArrayList<>();
+        LoadStoresRepositoryUseCase.loadStoresRepository((cName) -> {
+            str.add(cName);
+            lock.countDown();
+        });
+
+        lock.await(20000, TimeUnit.MILLISECONDS);
+        assertEquals(StoresRepository.class, str.get(0));
     }
 }
--- app/src/test/java/com/example/nolo/ExampleUnitTest.java
+++ app/src/test/java/com/example/nolo/ExampleUnitTest.java
@@ -1,8 +1,23 @@
 package com.example.nolo;
 
+import static org.junit.Assert.assertEquals;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.nolo.interactors.LoadStoresRepositoryUseCase;
+import com.example.nolo.repositories.store.StoresRepository;
+import com.google.firebase.FirebaseApp;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +25,5 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+
 }
