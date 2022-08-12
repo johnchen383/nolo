f172e5e447b8b99ae06851aae2498dc1a1663092
Nick <69305722+Bac0nEater@users.noreply.github.com>
2022-08-12 13:05:09 +1200

    AB#24 fix: accidentally remove imports

--- app/src/main/java/com/example/nolo/activities/MainActivity.java
+++ app/src/main/java/com/example/nolo/activities/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.example.nolo.interactors.LoadStoresRepositoryUseCase;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends BaseActivity {
     private ViewHolder vh;
