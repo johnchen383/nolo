b2bf19ccbfaee9d8143737cc6c5d4c3d6c09cb72
Nick <thenickys123@gmail.com>
2022-08-13 23:48:36 +1200

    AB#49 feat: implement ItemRepo interface

--- app/src/main/java/com/example/nolo/repositories/item/IItemsRepository.java
+++ app/src/main/java/com/example/nolo/repositories/item/IItemsRepository.java
@@ -1,4 +1,12 @@
 package com.example.nolo.repositories.item;
 
+import com.example.nolo.entities.item.IItem;
+
+import java.util.List;
+import java.util.function.Consumer;
+
 public interface IItemsRepository {
+    void loadItems(Consumer<Class<?>> loadedRepository);
+    List<IItem> getAllItems();
+    IItem getItemById(String itemId);
 }
--- app/src/main/java/com/example/nolo/repositories/item/ItemsRepository.java
+++ app/src/main/java/com/example/nolo/repositories/item/ItemsRepository.java
@@ -1,20 +1,30 @@
 package com.example.nolo.repositories.item;
 
+import com.example.nolo.entities.item.IItem;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * This is a singleton class for Items repository.
  */
 public class ItemsRepository implements IItemsRepository {
+    public static final long TIME_IN_MILLISECONDS_TEN_MINUTES = 1000*60*10;
     public static final String COLLECTION_PATH_LAPTOPS = "laptops";
     public static final String COLLECTION_PATH_PHONES = "phones";
     public static final String COLLECTION_PATH_ACCESSORIES = "accessories";
 
     private static ItemsRepository itemsRepository = null;
     private final FirebaseFirestore db;
+    private final List<IItem> itemsRepo;
+    private long lastLoadedTime;
 
     private ItemsRepository() {
         db = FirebaseFirestore.getInstance();
+        itemsRepo = new ArrayList<>();
+        lastLoadedTime = 0;
     }
 
     public static ItemsRepository getInstance() {
@@ -22,5 +32,20 @@ public class ItemsRepository implements IItemsRepository {
             itemsRepository = new ItemsRepository();
 
         return itemsRepository;
+    }
+
+    @Override
+    public void loadItems(Consumer<Class<?>> loadedRepository) {
+
+    }
+
+    @Override
+    public List<IItem> getAllItems() {
+        return null;
+    }
+
+    @Override
+    public IItem getItemById(String itemId) {
+        return null;
     }
 }
