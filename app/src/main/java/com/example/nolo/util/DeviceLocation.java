474da68087c14efa70a0d6cd3546757693d0e2f5
johnchen383 <john.bm.chen@gmail.com>
2022-08-18 11:27:00 +1200

    feat: added caching to location

--- app/src/main/java/com/example/nolo/util/DeviceLocation.java
+++ app/src/main/java/com/example/nolo/util/DeviceLocation.java
@@ -15,15 +15,19 @@ import com.google.android.gms.location.LocationServices;
 import java.util.function.Consumer;
 
 public class DeviceLocation {
+    public static final long TIME_IN_MILLISECONDS_FIVE_MINUTES = 1000*60*5;
     private static FusedLocationProviderClient fusedLocationClient;
     private static Location cachedLocation = null;
+    private static long lastLoadedTime = 0;
+    private static android.content.Context ctx;
 
     public static boolean hasLocationPermissions(@NonNull android.content.Context context) {
         return !(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED);
     }
 
-    public static void loadCurrentLocation(android.content.Context context) {
+    public static void loadCurrentLocation(@NonNull android.content.Context context) {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        ctx = context;
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
@@ -34,6 +38,13 @@ public class DeviceLocation {
     }
 
     public static Location getCurrentLocation(){
+        reloadCurrentLocationIfExpired();
+
         return cachedLocation;
+    }
+
+    private static void reloadCurrentLocationIfExpired() {
+        if (System.currentTimeMillis() - lastLoadedTime > TIME_IN_MILLISECONDS_FIVE_MINUTES)
+            loadCurrentLocation(ctx);
     }
 }
