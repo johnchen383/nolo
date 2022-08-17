a916022e1f03a7a6a8c97ce7ac204264897e28d9
johnchen383 <john.bm.chen@gmail.com>
2022-08-17 15:56:51 +1200

    doc: added interactor comments

--- app/src/main/java/com/example/nolo/interactors/AddCartItemUseCase.java
+++ app/src/main/java/com/example/nolo/interactors/AddCartItemUseCase.java
@@ -5,7 +5,7 @@ import com.example.nolo.repositories.user.UsersRepository;
 
 public class AddCartItemUseCase {
     /**
-     *
+     * Adds cart item to cart of current user, and pushes to firebase
      * @param cartItem
      */
     public static void addCart(IPurchasable cartItem) {
--- app/src/main/java/com/example/nolo/interactors/AddViewedItemUseCase.java
+++ app/src/main/java/com/example/nolo/interactors/AddViewedItemUseCase.java
@@ -5,7 +5,8 @@ import com.example.nolo.repositories.user.UsersRepository;
 
 public class AddViewedItemUseCase {
     /**
-     *
+     * records a selected itemvariant as being viewed in the view history of the current user, and pushes
+     * to firebase
      * @param item
      */
     public static void addViewHistory(IItemVariant item) {
