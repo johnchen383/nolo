ae8a873ac88b9ee7cdb56fafd9508e213c165497
Serena Lau (slau981) <serena.lau272@gmail.com>
2022-08-18 15:55:28 +1200

    fix: re-implement touchable components to be wrapped in container with minimum touch target dimensions

--- app/src/main/java/com/example/nolo/activities/LogInActivity.java
+++ app/src/main/java/com/example/nolo/activities/LogInActivity.java
@@ -7,6 +7,7 @@ import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.core.app.ActivityOptionsCompat;
@@ -17,6 +18,8 @@ import com.example.nolo.util.Animation;
 import com.example.nolo.viewmodels.LogInViewModel;
 import com.example.nolo.viewmodels.SplashViewModel;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LogInActivity extends BaseActivity {
     private LogInViewModel logInViewModel;
@@ -24,29 +27,30 @@ public class LogInActivity extends BaseActivity {
     private ViewHolder vh;
 
     private class ViewHolder {
-        EditText emailInput;
+        TextInputEditText emailInput;
         EditText passwordInput;
-        ImageView eyeBtn;
-        TextView forgotPassword;
-        TextView signUp;
-        MaterialButton logIn;
-        MaterialButton logInGoogle;
-
+        RelativeLayout eyeBtn, signUp;
+        ImageView eyeIcon;
+        TextView forgotPassword, signUpText;
+        MaterialButton logIn, logInGoogle;
 
         public ViewHolder(){
             emailInput = findViewById(R.id.email_edit);
             passwordInput = findViewById(R.id.password_edit);
-            eyeBtn = findViewById(R.id.eye_open);
+            eyeBtn = findViewById(R.id.eye_btn);
+            eyeIcon = findViewById(R.id.eye_icon);
             forgotPassword = findViewById(R.id.forgot_text_view);
             logIn = findViewById(R.id.login_button);
             logInGoogle = findViewById(R.id.login_google_button);
-            signUp = findViewById(R.id.register_text_view);
+            signUp = findViewById(R.id.register_btn);
+            signUpText = findViewById(R.id.register_text);
         }
     }
 
     private void initStyling() {
         vh.passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        vh.signUp.setPaintFlags(vh.signUp.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+        vh.signUpText.setPaintFlags(vh.signUpText.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+
     }
 
     private void initListeners() {
@@ -55,10 +59,10 @@ public class LogInActivity extends BaseActivity {
             System.out.println(vh.passwordInput.getInputType());
             if (isHidden) {
                 vh.passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                vh.eyeBtn.setImageResource(R.drawable.login_icon_eye_closed);
+                vh.eyeIcon.setImageResource(R.drawable.login_icon_eye_closed);
             } else {
                 vh.passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                vh.eyeBtn.setImageResource(R.drawable.login_icon_eye_open);
+                vh.eyeIcon.setImageResource(R.drawable.login_icon_eye_open);
             }
         });
 
@@ -73,6 +77,10 @@ public class LogInActivity extends BaseActivity {
         vh.logIn.setOnClickListener(v -> {
             String userEmail = vh.emailInput.getText().toString();
             String userPassword = vh.passwordInput.getText().toString();
+
+            if (userEmail.isEmpty() || userPassword.isEmpty()) {
+                return;
+            }
 
             System.out.println("Email: " + userEmail + "\nPassword: " + userPassword);
 
