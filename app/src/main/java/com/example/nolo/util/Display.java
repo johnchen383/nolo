9a807ede720163cc40f2e63ef03fff9a74aba1bb
johnchen383 <john.bm.chen@gmail.com>
2022-08-19 19:32:16 +1200

    doc: added documentation

--- app/src/main/java/com/example/nolo/util/Display.java
+++ app/src/main/java/com/example/nolo/util/Display.java
@@ -5,6 +5,11 @@ import android.util.DisplayMetrics;
 import android.view.View;
 
 public class Display {
+    /**
+     * Retrieve display metrics
+     * @param v any view associated with an activity (i.e., displayed)
+     * @return
+     */
     private static DisplayMetrics getDisplayMetrics(View v){
         DisplayMetrics displayMetrics = new DisplayMetrics();
 
@@ -15,10 +20,21 @@ public class Display {
         return displayMetrics;
     }
 
+    /**
+     * Get height of device screen
+     * @param v any view associated with an activity (i.e., displayed)
+     * @return
+     */
     public static int getScreenHeight(View v){
         return getDisplayMetrics(v).heightPixels;
     }
 
+    /**
+     * Get width of device screen
+     * @param v any view associated with an activity (i.e., displayed)
+     * @return
+     */
+    //TODO: may not be needed (delete if not!!)
     public static int getScreenWidth(View v){
         return getDisplayMetrics(v).widthPixels;
     }
