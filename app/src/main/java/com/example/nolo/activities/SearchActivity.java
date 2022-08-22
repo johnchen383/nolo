18f6bcee2cc8273b3fa63f8f4de12896b6ff9a1c
Nick <thenickys123@gmail.com>
2022-08-22 21:41:43 +1200

    feature: implement adaptor in search activity

--- app/src/main/java/com/example/nolo/activities/SearchActivity.java
+++ app/src/main/java/com/example/nolo/activities/SearchActivity.java
@@ -1,15 +1,23 @@
 package com.example.nolo.activities;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.nolo.R;
+import com.example.nolo.adaptors.SearchItemResultAdaptor;
+import com.example.nolo.entities.item.IItem;
+import com.example.nolo.interactors.item.GetSearchSuggestionsUseCase;
+import com.example.nolo.util.ListUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends BaseActivity {
     private ViewHolder vh;
-
 
     private class ViewHolder {
         EditText searchBarText;
@@ -29,10 +37,61 @@ public class SearchActivity extends BaseActivity {
         setContentView(R.layout.activity_search);
 
         vh = new ViewHolder();
+        initAdaptors();
         initListeners();
     }
 
-    private void initListeners() {
+    private void initAdaptors() {
+        // TODO: Result list at the start of the activity
+    }
 
+    private void resetSearchResults(String searchTerm) {
+        /**
+         * SEARCH SUGGESTION ADAPTOR
+         */
+        SearchItemResultAdaptor searchItemResultAdaptor;
+        List<IItem> searchResult = new ArrayList<>();
+
+        if (!searchTerm.isEmpty()) {
+            // First limit the number of items showing in the list
+            searchResult = GetSearchSuggestionsUseCase.getSearchSuggestions(searchTerm);
+        }
+
+        // Create and Set the adaptor
+        searchItemResultAdaptor = new SearchItemResultAdaptor(this, R.layout.item_search_suggestion, searchResult);
+        vh.searchResultList.setAdapter(searchItemResultAdaptor);
+        ListUtil.setDynamicHeight(vh.searchResultList);
+
+        // Set the result found text
+        String resultFoundMsg = searchResult.size() + " " + getString(R.string.search_results_found);
+        vh.numOfResultFound.setText(resultFoundMsg);
+    }
+
+    private void initListeners() {
+        vh.searchBarText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                resetSearchResults(s.toString());
+            }
+        });
+
+        vh.searchBarText.removeTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                resetSearchResults(s.toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nolo/adaptors/SearchItemResultAdaptor.java
+++ app/src/main/java/com/example/nolo/adaptors/SearchItemResultAdaptor.java
@@ -1,7 +1,6 @@
 package com.example.nolo.adaptors;
 
 import android.content.Context;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
