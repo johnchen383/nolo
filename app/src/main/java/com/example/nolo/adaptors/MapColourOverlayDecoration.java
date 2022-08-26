59dc8909cc43d6c4a32757d1a8ca2d00025b8f51
Nick <thenickys123@gmail.com>
2022-08-27 01:57:20 +1200

    feature: in map activity, show what colours are avaiable in each store

---
+++ app/src/main/java/com/example/nolo/adaptors/MapColourOverlayDecoration.java
@@ -0,0 +1,16 @@
+package com.example.nolo.adaptors;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MapColourOverlayDecoration extends RecyclerView.ItemDecoration {
+    private final static int vertOverlap = -80;
+
+    @Override
+    public void getItemOffsets (Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        outRect.set(0, 0, vertOverlap, 0);
+    }
+}
