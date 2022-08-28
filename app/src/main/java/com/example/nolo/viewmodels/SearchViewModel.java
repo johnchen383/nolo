c87ef2c925301286092c9933f8428a4b0aacc84a
Nick <thenickys123@gmail.com>
2022-08-28 21:26:05 +1200

    Revert "refactor: remove unneeded classes, SearchVM"
    This reverts commit ec30595c9021dd151403d68431f02e9e59a14db0.

---
+++ app/src/main/java/com/example/nolo/viewmodels/SearchViewModel.java
@@ -0,0 +1,9 @@
+package com.example.nolo.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel implements ISearchViewModel {
+
+    public SearchViewModel() {
+    }
+}
